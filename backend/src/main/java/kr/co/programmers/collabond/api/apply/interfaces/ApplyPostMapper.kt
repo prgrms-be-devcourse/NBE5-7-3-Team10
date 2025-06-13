c6c1bcd6292f84fe051e0f2f218cae62dbafc615
2ternal <ddft97@gmail.com>
2025-06-13 09:48:12 +0900

    Refactor: ApplyPostMapper 마이그레이션

---
+++ backend/src/main/java/kr/co/programmers/collabond/api/apply/interfaces/ApplyPostMapper.kt
@@ -0,0 +1,51 @@
+package kr.co.programmers.collabond.api.apply.interfaces
+
+import kr.co.programmers.collabond.api.apply.domain.ApplyPost
+import kr.co.programmers.collabond.api.apply.domain.ApplyPostStatus
+import kr.co.programmers.collabond.api.apply.domain.dto.ApplyPostDto
+import kr.co.programmers.collabond.api.apply.domain.dto.ApplyPostRequestDto
+import kr.co.programmers.collabond.api.attachment.domain.Attachment
+import kr.co.programmers.collabond.api.attachment.interfaces.AttachmentMapper
+import kr.co.programmers.collabond.api.profile.domain.Profile
+import kr.co.programmers.collabond.api.profile.interfaces.ProfileMapper
+import kr.co.programmers.collabond.api.recruit.domain.RecruitPost
+import kr.co.programmers.collabond.api.recruit.interfaces.RecruitPostMapper
+
+object ApplyPostMapper {
+
+    fun toEntity(
+        recruitPost: RecruitPost,
+        profile: Profile,
+        request: ApplyPostRequestDto
+    ): ApplyPost {
+        return ApplyPost(
+            recruitPost = recruitPost,
+            profile = profile,
+            content = request.content,
+            status = ApplyPostStatus.PENDING,
+        )
+    }
+
+    fun toDto(entity: ApplyPost): ApplyPostDto {
+        return ApplyPostDto(
+            id = entity.id,
+            recruitPost = RecruitPostMapper.toDto(entity.recruitPost),
+            profile = ProfileMapper.toDto(entity.profile),
+            content = entity.content,
+            status = entity.status.toString(),
+            attachmentFiles = (
+                    if (entity.attachments == null)
+                        null
+                    else
+                        entity.attachments!!
+                            .map { attachment: Attachment? ->
+                                AttachmentMapper.toDto(
+                                    attachment
+                                )
+                            }
+                            .toMutableList()
+                    ),
+            createdAt = entity.createdAt
+        )
+    }
+}
