5232bfaf846cc83db1ffdde424f1b58f7abdf34d
2ternal <ddft97@gmail.com>
2025-06-13 09:42:39 +0900

    Refactor: ApplyPost Entity 마이그레이션

---
+++ backend/src/main/java/kr/co/programmers/collabond/api/apply/domain/ApplyPost.kt
@@ -0,0 +1,52 @@
+package kr.co.programmers.collabond.api.apply.domain
+
+import jakarta.persistence.*
+import kr.co.programmers.collabond.api.attachment.domain.Attachment
+import kr.co.programmers.collabond.api.profile.domain.Profile
+import kr.co.programmers.collabond.api.recruit.domain.RecruitPost
+import kr.co.programmers.collabond.shared.domain.OnlyCreatedEntity
+
+@Entity
+@Table(name = "apply_posts")
+class ApplyPost(
+    @field:JoinColumn(
+        name = "recruit_post_id",
+        nullable = false
+    )
+    @field:ManyToOne(fetch = FetchType.LAZY)
+    var recruitPost: RecruitPost,
+
+    @field:JoinColumn(name = "profile_id")
+    @field:ManyToOne(fetch = FetchType.LAZY)
+    var profile: Profile,
+
+    @field:Column(
+        nullable = false,
+        length = 1000
+    )
+    var content: String,
+
+    @field:Column(nullable = false)
+    @field:Enumerated(EnumType.STRING)
+    var status: ApplyPostStatus,
+
+    @OneToMany(
+        mappedBy = "applyPost",
+        cascade = [CascadeType.ALL]
+    )
+    var attachments: MutableList<Attachment>? = null
+
+) : OnlyCreatedEntity() {
+
+    // 연관관계 편의 메서드
+    fun updateAttachment(attachments: MutableList<Attachment>) {
+        this.attachments = attachments
+        for (attachment in attachments) {
+            attachment.addApplyPost(this)
+        }
+    }
+
+    fun updateStatus(status: ApplyPostStatus) {
+        this.status = status
+    }
+}
