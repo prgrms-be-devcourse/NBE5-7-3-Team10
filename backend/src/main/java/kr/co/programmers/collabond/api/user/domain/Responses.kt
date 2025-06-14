07c58439d7915d81c929db934487b71098f9e588
sjsk3232 <sjsk3232@gmail.com>
2025-06-15 00:27:53 +0900

    Refactor: 리프레시 토큰 Entity, DTO 마이그레이션

--- backend/src/main/java/kr/co/programmers/collabond/api/user/domain/Responses.kt
+++ backend/src/main/java/kr/co/programmers/collabond/api/user/domain/Responses.kt
@@ -3,10 +3,16 @@ package kr.co.programmers.collabond.api.user.domain
 import java.time.LocalDateTime
 
 data class UserResponseDto(
-    val id: Long? = null,
-    val email: String? = null,
-    val nickname: String? = null,
-    val role: String? = null,
-    val createdAt: LocalDateTime? = null,
-    val updatedAt: LocalDateTime? = null
+    val id: Long,
+    val email: String,
+    val nickname: String,
+    val role: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
+)
+
+data class SignUpResponseDto(
+    val nickname: String,
+    val role: String,
+    val accessToken: String,
 )
