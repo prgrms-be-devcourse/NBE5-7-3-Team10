6130175444f46a2ae0f63a33486802b113a79ee1
sjsk3232 <sjsk3232@gmail.com>
2025-06-15 00:33:30 +0900

    Refactor: User 회원가입 로직 수정
    회원가입 시, role이 업데이트된 엑세스 토큰 발급 로직 추가

--- backend/src/main/java/kr/co/programmers/collabond/api/user/interfaces/UserMapper.kt
+++ backend/src/main/java/kr/co/programmers/collabond/api/user/interfaces/UserMapper.kt
@@ -1,5 +1,6 @@
 package kr.co.programmers.collabond.api.user.interfaces
 
+import kr.co.programmers.collabond.api.user.domain.SignUpResponseDto
 import kr.co.programmers.collabond.api.user.domain.User
 import kr.co.programmers.collabond.api.user.domain.UserResponseDto
 
@@ -10,4 +11,10 @@ fun toResponseDto(user: User) = UserResponseDto(
     role = user.role.name,
     createdAt = user.createdAt,
     updatedAt = user.updatedAt
+)
+
+fun toSignUpResponseDto(user: User, accessToken: String) = SignUpResponseDto(
+    nickname = user.nickname,
+    role = user.role.name,
+    accessToken = accessToken
 )
