8a3b526098dff1e5162c57172b519e57353210f8
2ternal <ddft97@gmail.com>
2025-06-17 03:04:36 +0900

    Refactor: ApplyPostMapper를 더 Kotlin스럽게 리팩토링

--- backend/src/main/java/kr/co/programmers/collabond/api/apply/interfaces/ApplyPostMapper.kt
+++ backend/src/main/java/kr/co/programmers/collabond/api/apply/interfaces/ApplyPostMapper.kt
@@ -4,7 +4,6 @@ import kr.co.programmers.collabond.api.apply.domain.ApplyPost
 import kr.co.programmers.collabond.api.apply.domain.ApplyPostStatus
 import kr.co.programmers.collabond.api.apply.domain.dto.ApplyPostDto
 import kr.co.programmers.collabond.api.apply.domain.dto.ApplyPostRequestDto
-import kr.co.programmers.collabond.api.attachment.domain.Attachment
 import kr.co.programmers.collabond.api.attachment.interfaces.AttachmentMapper
 import kr.co.programmers.collabond.api.profile.domain.Profile
 import kr.co.programmers.collabond.api.profile.interfaces.ProfileMapper
@@ -17,35 +16,21 @@ object ApplyPostMapper {
         recruitPost: RecruitPost,
         profile: Profile,
         request: ApplyPostRequestDto
-    ): ApplyPost {
-        return ApplyPost(
-            recruitPost = recruitPost,
-            profile = profile,
-            content = request.content,
-            status = ApplyPostStatus.PENDING,
-        )
-    }
+    ): ApplyPost = ApplyPost(
+        recruitPost = recruitPost,
+        profile = profile,
+        content = request.content,
+        status = ApplyPostStatus.PENDING,
+    )
 
-    fun toDto(entity: ApplyPost): ApplyPostDto {
-        return ApplyPostDto(
-            id = entity.id,
-            recruitPost = RecruitPostMapper.toDto(entity.recruitPost),
-            profile = ProfileMapper.toDto(entity.profile),
-            content = entity.content,
-            status = entity.status.toString(),
-            attachmentFiles = (
-                    if (entity.attachments == null)
-                        null
-                    else
-                        entity.attachments!!
-                            .map { attachment: Attachment ->
-                                AttachmentMapper.toDto(
-                                    attachment
-                                )
-                            }
-                            .toMutableList()
-                    ),
-            createdAt = entity.createdAt
-        )
-    }
+    fun toDto(entity: ApplyPost): ApplyPostDto = ApplyPostDto(
+        id = entity.id,
+        recruitPost = RecruitPostMapper.toDto(entity.recruitPost),
+        profile = ProfileMapper.toDto(entity.profile),
+        content = entity.content,
+        status = entity.status.name,
+        attachmentFiles = entity.attachments?.map { AttachmentMapper.toDto(it) }?.toMutableList(),
+        createdAt = entity.createdAt
+    )
+
 }
