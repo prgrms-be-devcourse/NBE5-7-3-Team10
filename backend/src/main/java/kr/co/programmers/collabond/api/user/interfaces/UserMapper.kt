7f96d5df8427440e26adb780b0c47e896210fcc5
Yesung-Lim <sjsk3232@gmail.com>
2025-06-17 15:46:40 +0900

    Refactor: Mapper 구조 변경

--- backend/src/main/java/kr/co/programmers/collabond/api/user/interfaces/UserMapper.kt
+++ backend/src/main/java/kr/co/programmers/collabond/api/user/interfaces/UserMapper.kt
@@ -4,17 +4,19 @@ import kr.co.programmers.collabond.api.user.domain.SignUpResponseDto
 import kr.co.programmers.collabond.api.user.domain.User
 import kr.co.programmers.collabond.api.user.domain.UserResponseDto
 
-fun toResponseDto(user: User) = UserResponseDto(
-    id = user.id,
-    email = user.email,
-    nickname = user.nickname,
-    role = user.role.name,
-    createdAt = user.createdAt,
-    updatedAt = user.updatedAt
-)
+object UserMapper {
+    fun toResponseDto(user: User) = UserResponseDto(
+        id = user.id,
+        email = user.email,
+        nickname = user.nickname,
+        role = user.role.name,
+        createdAt = user.createdAt,
+        updatedAt = user.updatedAt
+    )
 
-fun toSignUpResponseDto(user: User, accessToken: String) = SignUpResponseDto(
-    nickname = user.nickname,
-    role = user.role.name,
-    accessToken = accessToken
-)
+    fun toSignUpResponseDto(user: User, accessToken: String) = SignUpResponseDto(
+        nickname = user.nickname,
+        role = user.role.name,
+        accessToken = accessToken
+    )
+}
--- backend/src/main/java/kr/co/programmers/collabond/core/auth/jwt/TokenMapper.kt
+++ backend/src/main/java/kr/co/programmers/collabond/core/auth/jwt/TokenMapper.kt
@@ -3,22 +3,23 @@ package kr.co.programmers.collabond.core.auth.jwt
 import kr.co.programmers.collabond.api.user.domain.Role
 import kr.co.programmers.collabond.api.user.domain.User
 
-fun toTokenBodyDto(sub: String, role: String): TokenBodyDto =
-    TokenBodyDto(
-        providerId = sub,
-        role = Role.valueOf(role.uppercase())
-    )
+object TokenMapper {
+    fun toTokenBodyDto(sub: String, role: String): TokenBodyDto =
+        TokenBodyDto(
+            providerId = sub,
+            role = Role.valueOf(role.uppercase())
+        )
 
-fun toLoginTokenResponseDto(
-    accessToken: String,
-    refreshToken: String,
-    user: User
-): LoginTokenResponseDto =
-    LoginTokenResponseDto(
-        accessToken = accessToken,
-        refreshToken = refreshToken,
-        id = user.id,
-        nickname = user.nickname,
-        role = user.role
-    )
-
+    fun toLoginTokenResponseDto(
+        accessToken: String,
+        refreshToken: String,
+        user: User
+    ): LoginTokenResponseDto =
+        LoginTokenResponseDto(
+            accessToken = accessToken,
+            refreshToken = refreshToken,
+            id = user.id,
+            nickname = user.nickname,
+            role = user.role
+        )
+}
