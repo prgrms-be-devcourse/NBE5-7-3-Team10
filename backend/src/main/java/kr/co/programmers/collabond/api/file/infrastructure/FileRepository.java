9dc6f810dfbab94082f7bfe0a01cd196e9480bbc
2ternal <ddft97@gmail.com>
2025-06-17 15:45:00 +0900

    Refactor: FileRepository 마이그레이션

--- backend/src/main/java/kr/co/programmers/collabond/api/file/infrastructure/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.programmers.collabond.api.file.infrastructure;
-
-import kr.co.programmers.collabond.api.file.domain.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {
-}
