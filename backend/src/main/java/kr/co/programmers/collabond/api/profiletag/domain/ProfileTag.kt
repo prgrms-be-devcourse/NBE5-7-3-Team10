e8f6fa81768c3a70479c68b3c00133afbb79e59c
Yewon <ky1nonly@gmail.com>
2025-06-17 12:43:17 +0900

    Fix: ProfileTag 엔티티 수정

--- backend/src/main/java/kr/co/programmers/collabond/api/profiletag/domain/ProfileTag.kt
+++ backend/src/main/java/kr/co/programmers/collabond/api/profiletag/domain/ProfileTag.kt
@@ -4,26 +4,27 @@ import jakarta.persistence.*
 import kr.co.programmers.collabond.api.profile.domain.Profile
 import kr.co.programmers.collabond.api.tag.domain.Tag
 
+
 @Entity
 @Table(name = "profile_tags")
-class ProfileTag() {
+class ProfileTag(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "profile_id", nullable = false)
-    private var profile: Profile? = null
+    var profile: Profile? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tag_id", nullable = false)
-    var tag: Tag? = null
+    val tag: Tag,
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private val id: Long? = null
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+) {
 
-    protected constructor(dummy: Boolean) : this()
+    constructor(tag: Tag) : this(null, tag)
 
-    constructor(tag: Tag) : this() {
-        this.tag = tag
-    }
-    fun updateProfile(profile: Profile) {
+    fun setProfile(profile: Profile) {
         this.profile = profile
     }
 }
+
